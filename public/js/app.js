7b14fa6bb91fb20f952a221fb511f624507e0242
rohit_jaiswal2 <rohit_jaiswal2@persistent.com>
2022-06-10 00:09:13 +0530

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,50 @@
+// const { response } = require("express");
+
+// console.log("Client side javascript file is loaded!");
+
+// //fetching data in client side with help of fetch api which runs only one client side
+// fetch("http://puzzle.mead.io/puzzle").then((response) => {
+//   response.json().then((data) => {
+//     console.log(data);
+//   });
+// });
+
+// //fetching data from local server by giving address
+// fetch("http://localhost:3000/weather?address=boston").then((response) => {
+//   response.json().then((data) => {
+//     if (data.error) {
+//       console.log(data.error);
+//     } else {
+//       console.log(data.location);
+//       console.log(data.forecast);
+//     }
+//   });
+// });
+console.log("Client side javascript file is loaded!");
+
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const location = search.value;
+
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
+
+  fetch("http://localhost:3000/weather?address=" + location).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          messageOne.textContent = data.error;
+        } else {
+          messageOne.textContent = data.location;
+          messageTwo.textContent = data.forecast;
+        }
+      });
+    }
+  );
+});
---
+++ src/app.js
@@ -0,0 +1,110 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+
+const app = express();
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// Setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Rohit Jaiswal",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Rohit Jaiswal",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This is some helpful text.",
+    title: "Help",
+    name: "Rohit Jaiswal",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address!",
+    });
+  }
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, loacation } = {}) => {
+      if (error) {
+        res.send({ error });
+      }
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          res.send({ error });
+        }
+        res.send({
+          forecast: forecastData,
+          loacation,
+          address: req.query.address,
+        });
+      });
+    }
+  );
+
+  //   res.send({
+  //     forecast: "It is snowing",
+  //     location: "Philadelphia",
+  //     address: req.query.address,
+  //   });
+});
+
+//Query string: it is added in the request url after adding a question mark in the key value form like http://localhost/3000/products?search=game&rating=5
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    //accessing seacrh key of query string
+    return res.send({
+      error: "You must provide a search term",
+    });
+  }
+
+  console.log(req.query.search);
+  res.send({
+    products: [],
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Rohit Jaiswal",
+    errorMessage: "Help article not found.",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Rohit Jaiswal",
+    errorMessage: "Page not found.",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is up on port 3000.");
+});
